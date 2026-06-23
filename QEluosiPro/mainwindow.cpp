da04e8c70436bd1174824e1f63d486541c5df3cc
MrZhou <MrZhou@163.com>
2026-06-23 14:05:58 +0800

    [新增] 绘制活动区域(绘画事件)

--- QEluosiPro/mainwindow.cpp
+++ QEluosiPro/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QPainter>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -18,7 +21,39 @@ MainWindow::MainWindow(QWidget *parent) :
 #endif
     QIcon icons(filename);
     setWindowIcon(icons);
+
+    /* 设置固定大小 */
+    setFixedSize(680, 680);
 }
+
+
+/* 绘制活动区域 */
+void MainWindow::drawRangeRegion(QPainter &painter)
+{
+    /* 创建一个画笔 */
+    QPen pen(Qt::black);
+    /* 设置画笔的宽度 */
+    pen.setWidth(3);
+    /* 设置画笔风格为虚线 */
+    pen.setStyle(Qt::DashLine);
+
+    /* 画笔应用到QPainter */
+    painter.setPen(pen);
+
+    /* 绘制矩形 */
+    painter.drawRect(m_margin, m_margin, 480, 640);
+}
+
+/* 绘画事件  */
+void MainWindow::paintEvent(QPaintEvent *e)
+{
+    qDebug() << "paintEvent" << endl;
+
+    QPainter painter(this);
+
+    drawRangeRegion(painter);
+}
+
 
 MainWindow::~MainWindow()
 {
--- QEluosiPro/mainwindow.h
+++ QEluosiPro/mainwindow.h
@@ -15,8 +15,20 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    /* 绘画事件  */
+    void paintEvent(QPaintEvent *e) override;
+
+
+private:
+    /* 绘制活动区域 */
+    void drawRangeRegion(QPainter & painter);
+
 private:
     Ui::MainWindow *ui;
+
+    /* 边缘 */
+    static const int m_margin = 20;
 };
 
 #endif // MAINWINDOW_H
