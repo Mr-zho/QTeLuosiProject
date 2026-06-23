99d1c6c3b98630446292946880dede1bd8f4c2f1
MrZhou <MrZhou@163.com>
2026-06-23 19:22:03 +0800

    [新增] 绘制网格线

--- QEluosiPro/mainwindow.cpp
+++ QEluosiPro/mainwindow.cpp
@@ -44,6 +44,35 @@ void MainWindow::drawRangeRegion(QPainter &painter)
     painter.drawRect(m_margin, m_margin, 480, 640);
 }
 
+/* 绘制网格线 */
+void MainWindow::drawGridRegion(QPainter & painter)
+{
+    /* 创建一个画笔 */
+    QPen pen(Qt::gray);
+    /* 设置画笔的宽度 */
+    pen.setWidth(2);
+    /* 设置画笔风格为虚线 */
+    pen.setStyle(Qt::DashLine);
+
+    /* 画笔应用到QPainter */
+    painter.setPen(pen);
+
+    /* 绘制垂直的网格线 */
+    for (int x = 0; x <= BOARD_WIDTH; x++)
+    {
+        int lineX = m_margin + x * 32;
+        painter.drawLine(lineX, m_margin, lineX, m_margin + BOARD_HEIGHT * 32);
+    }
+
+    /* 绘制水平的网格线 */
+    for (int y = 0; y <= BOARD_HEIGHT; y++)
+    {
+        int lineY = m_margin + y * 32;
+        painter.drawLine(m_margin, lineY, m_margin + BOARD_WIDTH * 32, lineY);
+    }
+
+}
+
 /* 绘画事件  */
 void MainWindow::paintEvent(QPaintEvent *e)
 {
@@ -51,7 +80,10 @@ void MainWindow::paintEvent(QPaintEvent *e)
 
     QPainter painter(this);
 
+    /* 绘制活动区域 */
     drawRangeRegion(painter);
+    /* 绘制网格线 */
+    drawGridRegion(painter);
 }
 
 
--- QEluosiPro/mainwindow.h
+++ QEluosiPro/mainwindow.h
@@ -23,12 +23,16 @@ protected:
 private:
     /* 绘制活动区域 */
     void drawRangeRegion(QPainter & painter);
-
+    /* 绘制网格线 */
+    void drawGridRegion(QPainter & painter);
 private:
     Ui::MainWindow *ui;
 
     /* 边缘 */
     static const int m_margin = 20;
+
+    static const int BOARD_WIDTH = 480 / 32;
+    static const int BOARD_HEIGHT = 640 / 32;
 };
 
 #endif // MAINWINDOW_H
