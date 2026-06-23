610d756b190ddf678b0705e4bc213adc3d67cfd8
MrZhou <MrZhou@163.com>
2026-06-23 11:40:47 +0800

    [修改] 添加软件标题和软件图标

--- QEluosiPro/mainwindow.cpp
+++ QEluosiPro/mainwindow.cpp
@@ -6,6 +6,18 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    /* 设置软件的标题 */
+    setWindowTitle("俄罗斯方块");
+
+    /* 资料文件 */
+#if 0
+    const char * filename = "D:\\MyProject\\QTeLuosiProject\\QEluosiPro\\icons\\icon.png";
+#else
+    const char * filename = ":/icons/icon.png";
+#endif
+    QIcon icons(filename);
+    setWindowIcon(icons);
 }
 
 MainWindow::~MainWindow()
